433712618bd4205603c67c0dd63a55db7a2ab4a1
Bilal <taybib4@gmail.com>
2023-06-04 09:10:57 +0100

    first commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('app'),
+      ),
+      body: Text('welcmoe'),
+    );
+  }
+}
